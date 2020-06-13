be8359113f6df4d07eafba7e197729e2454388cb
zetahawke <michel.szinavel@gmail.com>
2020-06-12 21:56:41 -0400

    Adding basic form to add photos on products

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ApplicationRecord
   has_one :stock, dependent: :destroy
   has_many :product_tags, dependent: :destroy
   has_many :tags, through: :product_tags
+  has_one_attached :main_image
+  has_many_attached :secondary_images
 
   accepts_nested_attributes_for :stock
   accepts_nested_attributes_for :product_tags, allow_destroy: true
